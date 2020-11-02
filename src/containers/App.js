dbb944ed526f9289069ea3bdb0bda362ccee1c03
ckaylor33 <kaylor33@hotmail.co.uk>
2020-11-02 07:10:37 +0000

    hooks update 2

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,21 +5,16 @@ import Scroll from "../components/Scroll";
 import "./App.css";
 
 function App() {
-  //constructor() {
-  //  super()
-  //  this.state = {
-  //    robots: [],
-  //    searchfield: ''
-  //  }
-  //}
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState("");
 
-  //componentDidMount() {
-  //  fetch('https://jsonplaceholder.typicode.com/users')
-  //    .then(response=> response.json())
-  //    .then(users => {this.setState({ robots: users})});
-  //}
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setRobots(users);
+      });
+  }, []);
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
@@ -28,7 +23,6 @@ function App() {
   const filteredRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
   });
-  console.log(robots, searchfield);
   return !robots.length ? (
     <h1>Loading</h1>
   ) : (
