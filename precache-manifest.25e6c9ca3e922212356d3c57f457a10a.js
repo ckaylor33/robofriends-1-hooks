d1572dee3870bfd4c41ced060cbc46867b6bb1e0
ckaylor33 <kaylor33@hotmail.co.uk>
2020-11-03 06:14:44 +0000

    Updates

---
+++ precache-manifest.25e6c9ca3e922212356d3c57f457a10a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f070bcfc68c9bd4a4e5750f27518f03",
+    "url": "/ckaylor33/robofriends-1-hooks/index.html"
+  },
+  {
+    "revision": "1cbc188c79b5223e37ba",
+    "url": "/ckaylor33/robofriends-1-hooks/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "acd7d46ed932c3ec6b0c",
+    "url": "/ckaylor33/robofriends-1-hooks/static/css/main.c3e2e57f.chunk.css"
+  },
+  {
+    "revision": "1cbc188c79b5223e37ba",
+    "url": "/ckaylor33/robofriends-1-hooks/static/js/2.ac186811.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ckaylor33/robofriends-1-hooks/static/js/2.ac186811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd7d46ed932c3ec6b0c",
+    "url": "/ckaylor33/robofriends-1-hooks/static/js/main.87565a12.chunk.js"
+  },
+  {
+    "revision": "2d3122aa87edb6bc8e62",
+    "url": "/ckaylor33/robofriends-1-hooks/static/js/runtime-main.96e273ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ckaylor33/robofriends-1-hooks/static/media/SEGA.12a3db8b.woff"
+  }
+]);
